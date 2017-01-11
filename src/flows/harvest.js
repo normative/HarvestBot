f3afc1b62b92af8502ccb8b5800c458e252de6f9
Spencer Alderson <spenceralderson@gmail.com>
2017-01-11 14:43:50 -0500

    minor clean

--- src/flows/harvest.js
+++ src/flows/harvest.js
@@ -67,7 +67,7 @@ module.exports = (app) => {
                                     type: 'button',
                                     value: 'cancel',
                                     style: 'danger',
-                                }
+
                             }
                             ]
                         }
